3fe70b487d8c2005199a8a4e3a3946dea1a4f4ae
victorjimoh <80990646+victorjimoh@users.noreply.github.com>
2021-10-30 02:53:01 +0100

    update main js

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
     $(window).on('load',function(){
-<<<<<<< HEAD
-        $('.loader-wrapper').fadeOut(30000);
-=======
-        $('.loader-wrapper').fadeOut("slow");
->>>>>>> dd9d3bc6ef97aee20c790751541d6b8374348927
-   });
+       $('.loader-wrapper').fadeOut(30000);
+
+         });
   });
